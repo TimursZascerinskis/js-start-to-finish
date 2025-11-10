b18cc0a7622e4dd3594ccc0afaa0e51c6e1806b7
unknown <your@email.example>
2025-11-10 10:19:39 +0200

    1.uzz finish

--- 1.uzd/app.js
+++ 1.uzd/app.js
@@ -1,17 +1,33 @@
-    const start = document.getElementById("start");
-    const finish = document.getElementById("finish");
-    const submitNumbers = document.getElementById("submitNumbers");
+        const start = document.getElementById("start");
+        const finish = document.getElementById("finish");
+        const submitNumbers = document.getElementById("submitNumbers");
+        const resultDiv = document.getElementById("result");
+        const historyDiv = document.getElementById("history");
 
-    function startFinish() {
+        function saveHistory(startValue, finishValue, numbers) {
+            const historyItem = document.createElement("li");
+            historyItem.classList.add("history-item");
 
-        const startValue = parseInt(start.value);
-        const finishValue = parseInt(finish.value);
+            const historyText = document.createElement("span");
+            historyText.textContent = `${numbers.join(", ")}`;
 
-        let numbers = [];
-        for (let i = startValue; i <= finishValue; i++) {
-            numbers.push(i);
+            historyItem.appendChild(historyText);
+
+            historyDiv.appendChild(historyItem);
         }
 
-        document.getElementById("result").innerHTML =  numbers.join(", ");
-    }
-    submitNumbers.addEventListener("click", startFinish);
+        function startFinish() {
+            const startValue = parseInt(start.value);
+            const finishValue = parseInt(finish.value);
+
+            let numbers = [];
+            for (let i = startValue; i <= finishValue; i++) {
+                numbers.push(i);
+            }
+
+            resultDiv.innerHTML = numbers.join(", ");
+
+            saveHistory(startValue, finishValue, numbers);
+        }
+
+        submitNumbers.addEventListener("click", startFinish);
