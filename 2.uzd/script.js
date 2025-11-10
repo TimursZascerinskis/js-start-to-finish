c0ec465fb1eb130b0bdab141f674763ad41a4f8f
unknown <your@email.example>
2025-11-10 10:33:18 +0200

    end

--- 2.uzd/script.js
+++ 2.uzd/script.js
@@ -5,7 +5,14 @@ const resultDiv = document.getElementById("result");
 const kmi = document.getElementById("kmi");
 
 function KalkulatorKMI(){
-    
+    const ageValue = parseInt(age.value);
+    const svarsValue = parseInt(svars.value);
+    const garumsValue = parseInt(garums.value);
+    let kmiNumbers = [];
+    svarsValue/garumsValue^2 == kmiNumbers[i];
+
+    result.DivinnerHTML = kmiNumbers.join(", ");
+
 }
 
 kmi.addEventListener("click", KalkulatorKMI);
