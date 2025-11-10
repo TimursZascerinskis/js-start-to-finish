0bbf93bda6b471de20531b79961b27de801cd07b
unknown <your@email.example>
2025-11-10 10:27:36 +0200

    2.uzd sakums

---
+++ 2.uzd/script.js
@@ -0,0 +1,11 @@
+const age = document.getElementById("age");
+const svars = document.getElementById("svars");
+const garums = document.getElementById("garums");
+const resultDiv = document.getElementById("result");
+const kmi = document.getElementById("kmi");
+
+function KalkulatorKMI(){
+    
+}
+
+kmi.addEventListener("click", KalkulatorKMI);
